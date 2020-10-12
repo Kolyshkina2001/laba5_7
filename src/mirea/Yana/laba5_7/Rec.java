535c5779a5e25107e5f881c73affc710dbb9984b
yakol <ya.kolysh@gmail.com>
2020-10-12 16:35:49 +0300

    Initial commit

---
+++ src/mirea/Yana/laba5_7/Rec.java
@@ -0,0 +1,18 @@
+package mirea.Yana.laba5_7;
+
+public class Rec {
+    public int rec(long x, long div, double val) {
+        if (x == 1 || div > val) {
+            return 0;
+        }
+        else {
+            if (x % div == 0) {
+                System.out.print(" * " + div);
+                return rec(x / div, div, val);
+            }
+            else {
+                return rec(x, div + 1, val);
+            }
+        }
+    }
+}
